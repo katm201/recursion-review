42291dc06ff523170eb0155fc882ccd24fd6b8fb
katm201 <katm201@gmail.com>
2017-10-09 12:15:18 -0700

    Complete src/getElementsByClassName. All tests pass.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,40 +6,21 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className, node
 ) {
-  // your code here
-  //set default value for node
   node = node || document.body;
 
   var nodes = [];
-
   
-  //create function to check if the
-  //classname is correct
-  
-
   var checkClass = function(currentNode) {
-    //
+    return currentNode.classList.contains(className);
   };
-
-  //check if our current node has the correct
-  //classname
-    //if so, push node to nodes array
 
   if (checkClass(node)) {
     nodes.push(node);
-  }
-
-  //depth first search
-    //iterate over all child nodes
-    //recurse over each child node
+  } 
 
   for (var i = 0; i < node.children.length; i++) {
-    getElementsByClassName(className, node.children[i]);
+    nodes = nodes.concat(getElementsByClassName(className, node.children[i]));
   }
-  
+
   return nodes;
-  //return nodes array
 };
-
-
-//You should use document.body, element.childNodes, and element.classList
