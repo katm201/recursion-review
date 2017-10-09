b29a26633e0ceaaf459d7e7884e0a8851293a087
katm201 <katm201@gmail.com>
2017-10-09 16:15:44 -0700

    Taken care of cases of empty array and object

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,24 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+
+  var characters = json.split('');
+ 
+  if (characters[0] === '{') {
+    var obj = {};
+    if (characters.length === 2) {
+      return obj;
+    }
+
+  }
+  if (characters[0] === '[') {
+  
+    var arr = [];
+    if (characters.length === 2) {
+      return arr;
+    }
+
+  }
+  //return the obj that is built out
 };
