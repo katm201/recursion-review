1dc7f991cb6b5a44401c5ab48c69397163206cff
katm201 <katm201@gmail.com>
2017-10-09 16:40:59 -0700

    Parses empty arrays, objects and strings, outlining additional approaches

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -13,15 +13,36 @@ var parseJSON = function(json) {
     if (characters.length === 2) {
       return obj;
     }
-
-  }
-  if (characters[0] === '[') {
+    // characters.shift();
+    // characters.pop();
+    json = json.substring(1, json.length - 2);
+    //case 1: content of object is a primitive
+    // var pairs = json.split(',');
+    // return pairs;    
+  } else if (characters[0] === '[') {
   
     var arr = [];
     if (characters.length === 2) {
       return arr;
     }
 
+    //characters.shift();
+    //characters.pop();
+
+  } else if (characters[0] === '') {
+    // dealing with string
+    if (characters.length === 2) {
+      return '';
+    }
+    var counter = 0;
+    //iterate over the characters array to find out where
+    //the string ends
+    
+  } else {
+    //primitives--anything that doesn't start
+    //with [ or {
   }
+
   //return the obj that is built out
+  //return characters;
 };
